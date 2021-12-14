1185b2599a6c5754285e8e47c2db5cb5ed6d7cf2
Naima Hassan <naima.hassan434@gmail.com>
2021-12-14 20:25:31 +0000

    useEffect hook with clean-up

---
+++ src/components/ShowXY.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+
+function ShowXY() {
+  const [strXY, setStrXY] = useState("0,0");
+  const displayXY = (e) => {
+    let str = e.pageX + "," + e.pageY;
+    setStrXY(str);
+    console.log(str);
+  };
+  useEffect(() => {
+    window.addEventListener("mousemove", displayXY);
+    return () => {
+      window.removeEventListener("mousemove", displayXY);
+    };
+  });
+  return (
+    <div>
+      <h1>{strXY}</h1>
+    </div>
+  );
+}
+
+export default ShowXY;
