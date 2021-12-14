73cad02c36c5a1614708b2ae95f82707e46918de
Naima Hassan <naima.hassan434@gmail.com>
2021-12-14 21:08:29 +0000

    React.createRef method

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.iElem = React.createRef();
+  }
+  componentDidMount() {
+    this.iElem.current.focus();
+  }
   render() {
     return (
       <div className="App">
         <input type="text" />
         <hr />
-        <input type="text" ref={(elem) => (this.iElem = elem)} />
+        <input type="text" ref={this.iElem} />
         <hr />
-        <button onClick={() => this.iElem.focus()}>Set Focus</button>
+        <button onClick={() => this.iElem.current.focus()}>Set Focus</button>
       </div>
     );
   }
