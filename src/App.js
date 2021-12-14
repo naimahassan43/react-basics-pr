f96bfbde0965f546cd88e2997b9d8b823f97417b
Naima Hassan <naima.hassan434@gmail.com>
2021-12-14 20:56:58 +0000

    ref- the older way

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 
-import ShowXY from "./components/ShowXY";
-
-function App() {
-  const [show, setShow] = useState(true);
-  return (
-    <div className="App">
-      {show ? <ShowXY /> : <h1>No Data</h1>}
-      <button onChange={() => setShow(!show)}>Toggle</button>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <input type="text" />
+        <hr />
+        <input type="text" ref={(elem) => (this.iElem = elem)} />
+        <hr />
+        <button onClick={() => this.iElem.focus()}>Set Focus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
