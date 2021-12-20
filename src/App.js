42a157d3caa738f347cf9d6c123bd4f6be525d1c
Naima Hassan <naima.hassan434@gmail.com>
2021-12-20 10:01:39 +0000

    form-state as object

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function App() {
-  const getData = (e) => {
+  const [persInfo, setPersInfo] = useState({
+    fname: "",
+    lname: "",
+    phone: "",
+  });
+
+  const fNameHandler = (e) => {
+    setPersInfo({ ...persInfo, fname: e.target.value });
+  };
+  const lNameHandler = (e) => {
+    setPersInfo({ ...persInfo, lname: e.target.value });
+  };
+  const phoneHandler = (e) => {
+    setPersInfo({ ...persInfo, phone: e.target.value });
+  };
+  const showData = (e) => {
     e.preventDefault();
-    console.log("Submitted");
+    // setPersInfo(fname + " " + lname + "," + phone);
   };
   return (
     <div className="App">
-      <form action="" onSubmit={getData}>
-        <input type="text" />
-
-        <input type="text" />
-
+      <form action="" onSubmit={showData}>
+        <input type="text" placeholder="First Name" onChange={fNameHandler} />
+        <br />
+        <input type="text" placeholder="Last Name" onChange={lNameHandler} />
+        <br />
+        <input type="text" placeholder="Phone Number" onChange={phoneHandler} />
+        <br />
         <input type="submit" value="Submit" />
       </form>
+      {JSON.stringify(persInfo)}
     </div>
   );
 }
